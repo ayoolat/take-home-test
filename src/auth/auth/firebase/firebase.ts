30f141bfcd1b11737d77b7197d0befbed5729f6f
Toluwanimi <ayoola_toluwanimi@yahoo.com>
2024-02-13 01:22:22 +0100

    chore: cleaned up code and app secrets

--- src/auth/auth/firebase/firebase.ts
+++ src/auth/auth/firebase/firebase.ts
@@ -1,25 +1,24 @@
-import * as serviceAccount from './firebaseServiceAccount.json';
 import 'firebase/auth';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 const firebaseConfig = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientX509CertUrl: serviceAccount.client_x509_cert_url,
+  type: process.env.TYPE || '',
+  projectId: process.env.PROJECT_ID || '',
+  privateKeyId: process.env.PRIVATE_KEY_ID || '',
+  privateKey: process.env.PRIVATE_KEY || '',
+  clientEmail: process.env.CLIENT_EMAIL || '',
+  clientId: process.env.CLIENT_ID || '',
+  authUri: process.env.AUTH_URL || '',
+  tokenUri: process.env.TOKEN_URI || '',
+  authProviderX509CertUrl: process.env.auth_provider_x509_cert_url || '',
+  clientX509CertUrl: process.env.client_x509_cert_url || '',
 };
 
 const firebaseAuthConfig = {
   apiKey: process.env.API_KEY || '',
   authDomain: process.env.AUTH_DOMAIN || '',
-  projectId: serviceAccount.project_id,
+  projectId: process.env.PROJECT_ID || '',
   storageBucket: process.env.STORAGE_BUCKET || '',
   messagingSenderId: process.env.MESSAGING_SENDER_ID || '',
   appId: process.env.APP_ID || '',
